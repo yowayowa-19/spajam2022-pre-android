4bbd1f1590b01d9f8742fde5d22b616f90f114c0
Atria <atriasft@gmail.com>
2022-08-20 17:47:56 +0900

    add deploy.yml & 起動確認

--- app/src/main/java/spajam/yowayowa/mousyo/view/MainActivity.kt
+++ app/src/main/java/spajam/yowayowa/mousyo/view/MainActivity.kt
@@ -2,9 +2,14 @@ package spajam.yowayowa.mousyo.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import spajam.yowayowa.mousyo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
