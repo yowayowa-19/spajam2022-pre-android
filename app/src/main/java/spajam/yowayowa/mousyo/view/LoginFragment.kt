eaf8847c7445597edb369a89816628a851d6ca13
Atria <atriasft@gmail.com>
2022-08-21 06:03:47 +0900

    loginの実装

--- app/src/main/java/spajam/yowayowa/mousyo/view/LoginFragment.kt
+++ app/src/main/java/spajam/yowayowa/mousyo/view/LoginFragment.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
+import kotlinx.coroutines.runBlocking
 import spajam.yowayowa.mousyo.R
 import spajam.yowayowa.mousyo.databinding.FragmentLoginBinding
+import spajam.yowayowa.mousyo.repository.AccountRepository
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -23,8 +27,17 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
-        loginViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        val accountRepository = AccountRepository()
+        val factory = LoginViewModel.Factory(accountRepository)
+        loginViewModel = ViewModelProvider(this, factory)[LoginViewModel::class.java]
         val root = binding.root
+
+        loginViewModel.loginFailure.observe(
+            viewLifecycleOwner,
+            Observer {
+                if (it)binding.validationTextView.visibility = View.VISIBLE
+            }
+        )
 
         binding.editTextTextUserName.doOnTextChanged { text, start, count, after ->
             loginViewModel.setUsername(text.toString())
@@ -34,9 +47,13 @@ class LoginFragment : Fragment() {
         }
 
         binding.loginButton.setOnClickListener {
-            if (login(loginViewModel.usernameText.value, loginViewModel.passwordText.value)) {
-                startActivity(Intent(requireContext(), MainActivity::class.java))
-                activity?.finish()
+            runBlocking {
+                val result = loginViewModel.login()
+                Toast.makeText(context, "result : $result", Toast.LENGTH_SHORT).show()
+                if (result != -1) {
+                    startActivity(Intent(requireContext(), MainActivity::class.java))
+                    activity?.finish()
+                } else loginViewModel.loginFailed()
             }
         }
         binding.registerClickableTextView.setOnClickListener {
@@ -53,11 +70,5 @@ class LoginFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    // TODO:Implementation
-    private fun login(username: String?, password: String?): Boolean {
-        println("username : $username, password : $password")
-        return true
     }
 }
