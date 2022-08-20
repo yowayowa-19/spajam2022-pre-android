1517f791ec82ad781b77d09e648158235beacac0
Atria <atriasft@gmail.com>
2022-08-21 06:27:16 +0900

    暫定処理でMainActivityに戻る処理を追加

--- app/src/main/java/spajam/yowayowa/mousyo/view/LoginAcceptedFragment.kt
+++ app/src/main/java/spajam/yowayowa/mousyo/view/LoginAcceptedFragment.kt
@@ -1,9 +1,11 @@
 package spajam.yowayowa.mousyo.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import spajam.yowayowa.mousyo.databinding.FragmentLoginAcceptedBinding
 
@@ -18,6 +20,11 @@ class LoginAcceptedFragment : Fragment() {
     ): View {
         _binding = FragmentLoginAcceptedBinding.inflate(inflater, container, false)
         val root = binding.root
+        binding.button.setOnClickListener {
+            Toast.makeText(context, "とりあえずMainActivityに戻します", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(context, MainActivity::class.java))
+            activity?.finish()
+        }
         return root
     }
 
