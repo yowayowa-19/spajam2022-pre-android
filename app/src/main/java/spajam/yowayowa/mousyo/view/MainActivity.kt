54455af0c6c6b3e97d68eb470a5ba1b2de492847
Atria <atriasft@gmail.com>
2022-08-21 08:37:54 +0900

    非ログイン状態でMainActivityを起動した際、ログイン画面に飛ばすよう実装

--- app/src/main/java/spajam/yowayowa/mousyo/view/MainActivity.kt
+++ app/src/main/java/spajam/yowayowa/mousyo/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package spajam.yowayowa.mousyo.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,9 +10,11 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import spajam.yowayowa.mousyo.R
 import spajam.yowayowa.mousyo.databinding.ActivityMainBinding
+import spajam.yowayowa.mousyo.util.SharedPreferencesService
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferencesService: SharedPreferencesService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,5 +31,12 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // ログインしていない場合
+        sharedPreferencesService = SharedPreferencesService(this)
+        if (sharedPreferencesService.getUserId() == -1) {
+            startActivity(Intent(this, OnboardingActivity::class.java))
+            this.finish()
+        }
     }
 }
