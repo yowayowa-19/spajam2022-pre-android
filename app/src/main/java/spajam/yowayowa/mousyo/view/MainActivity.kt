806bb2904d6b8124abce90e5841c045b83ca26e8
Atria <atriasft@gmail.com>
2022-08-20 16:49:20 +0900

    init project

---
+++ app/src/main/java/spajam/yowayowa/mousyo/view/MainActivity.kt
@@ -0,0 +1,10 @@
+package spajam.yowayowa.mousyo.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
