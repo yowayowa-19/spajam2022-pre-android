97226f8db7e186cc10b6d4ebb86b3d58388d30c9
Atria <atriasft@gmail.com>
2022-08-21 14:04:01 +0900

    weeklyを最低限動くようにした

--- app/src/main/java/spajam/yowayowa/mousyo/view/mission/WeeklyMissionFragment.kt
+++ app/src/main/java/spajam/yowayowa/mousyo/view/mission/WeeklyMissionFragment.kt
@@ -1,16 +1,20 @@
 package spajam.yowayowa.mousyo.view.mission
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import spajam.yowayowa.mousyo.databinding.FragmentMissionTemplateBinding
+import spajam.yowayowa.mousyo.model.Mission
 
 class WeeklyMissionFragment : Fragment() {
 
-    private lateinit var rankingViewModel: WeeklyMissionViewModel
+    private lateinit var weekMissionViewModel: WeeklyMissionViewModel
     private var _binding: FragmentMissionTemplateBinding? = null
 
     // This property is only valid between onCreateView and
@@ -22,9 +26,33 @@ class WeeklyMissionFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        rankingViewModel = ViewModelProvider(this)[WeeklyMissionViewModel::class.java]
+        weekMissionViewModel = ViewModelProvider(this)[WeeklyMissionViewModel::class.java]
         _binding = FragmentMissionTemplateBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        weekMissionViewModel.missions.observe(
+            viewLifecycleOwner,
+            Observer {
+                binding.recyclerView.setHasFixedSize(true)
+                binding.recyclerView.layoutManager = LinearLayoutManager(context)
+                binding.recyclerView.adapter = DailyMissionAdapter(
+                    it,
+                    object : DailyMissionAdapter.ListListener {
+                        override fun onClickItem(tappedView: View, mission: Mission) {
+                            // ポップアップ表示
+                            val builder = AlertDialog.Builder(activity)
+                            builder.setTitle(mission.title)
+                                .setMessage("ミッションを完了しますか?")
+                                .setPositiveButton("完了") { dialog, id ->
+                                    // このボタンを押した時の処理を書きます。
+                                    weekMissionViewModel.deleteMission(mission)
+                                }
+                                .setNegativeButton("キャンセル", null)
+                                .show()
+                        }
+                    }
+                )
+            }
+        )
         return root
     }
 
--- app/src/main/java/spajam/yowayowa/mousyo/view/mission/WeeklyMissionViewModel.kt
+++ app/src/main/java/spajam/yowayowa/mousyo/view/mission/WeeklyMissionViewModel.kt
@@ -1,5 +1,30 @@
 package spajam.yowayowa.mousyo.view.mission
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import spajam.yowayowa.mousyo.model.Mission
 
-class WeeklyMissionViewModel : ViewModel()
+class WeeklyMissionViewModel : ViewModel() {
+    // Dailyコードそのまま...
+    private val _missions: MutableLiveData<List<Mission>> by lazy {
+        MutableLiveData<List<Mission>>().also {
+            it.value = loadMissionsMoq()
+        }
+    }
+    val missions: LiveData<List<Mission>>
+        get() = _missions
+    private fun loadMissionsMoq(): List<Mission> {
+        val mission1 = Mission(1, "エアコンを7時間つける", "", 1, 0, false)
+        val mission2 = Mission(2, "昼間に電気を10時間つける", "", 5, 0, false)
+        return listOf(mission1, mission2)
+    }
+    fun deleteMission(target: Mission) {
+        val mutableList = mutableListOf<Mission>()
+        _missions.value?.forEach {
+            mutableList.add(it)
+        }
+        mutableList.remove(target)
+        _missions.value = mutableList
+    }
+}
