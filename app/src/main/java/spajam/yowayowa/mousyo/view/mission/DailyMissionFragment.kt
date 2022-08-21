32c428c57d989db783b432b9ab95862bb9a16904
Atria <atriasft@gmail.com>
2022-08-21 11:50:17 +0900

    要素追加してUI改善

--- app/src/main/java/spajam/yowayowa/mousyo/view/mission/DailyMissionFragment.kt
+++ app/src/main/java/spajam/yowayowa/mousyo/view/mission/DailyMissionFragment.kt
@@ -7,14 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import spajam.yowayowa.mousyo.databinding.FragmentRankingTemplateBinding
+import spajam.yowayowa.mousyo.databinding.FragmentMissionTemplateBinding
 
 class DailyMissionFragment : Fragment() {
 
     private lateinit var dailyMissionViewModel: DailyMissionViewModel
-    private var _binding: FragmentRankingTemplateBinding? = null
+    private var _binding: FragmentMissionTemplateBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,14 +25,11 @@ class DailyMissionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         dailyMissionViewModel = ViewModelProvider(this)[DailyMissionViewModel::class.java]
-        _binding = FragmentRankingTemplateBinding.inflate(inflater, container, false)
+        _binding = FragmentMissionTemplateBinding.inflate(inflater, container, false)
         dailyMissionViewModel.missions.observe(viewLifecycleOwner, Observer {
             binding.recyclerView.setHasFixedSize(true)
             binding.recyclerView.layoutManager = LinearLayoutManager(context)
             binding.recyclerView.adapter = DailyMissionAdapter(it)
-            binding.recyclerView.addItemDecoration(
-                DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
-            )
         })
         val root: View = binding.root
         return root
--- app/src/main/java/spajam/yowayowa/mousyo/view/mission/WeeklyMissionFragment.kt
+++ app/src/main/java/spajam/yowayowa/mousyo/view/mission/WeeklyMissionFragment.kt
@@ -6,12 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import spajam.yowayowa.mousyo.databinding.FragmentRankingTemplateBinding
+import spajam.yowayowa.mousyo.databinding.FragmentMissionTemplateBinding
 
 class WeeklyMissionFragment : Fragment() {
 
     private lateinit var rankingViewModel: WeeklyMissionViewModel
-    private var _binding: FragmentRankingTemplateBinding? = null
+    private var _binding: FragmentMissionTemplateBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,7 +23,7 @@ class WeeklyMissionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         rankingViewModel = ViewModelProvider(this)[WeeklyMissionViewModel::class.java]
-        _binding = FragmentRankingTemplateBinding.inflate(inflater, container, false)
+        _binding = FragmentMissionTemplateBinding.inflate(inflater, container, false)
         val root: View = binding.root
         return root
     }
